aca6a4b7394d01ee773582cebde610d68c52ad37
Dan Clayton <clayton.daniel.j@gmail.com>
2019-08-22 12:00:22 -0600

    conditional and loop practice

---
+++ Exercises/loops-conditionals-practice/if-statement-olympics/app.js
@@ -0,0 +1,116 @@
+//prelim 
+
+//1
+
+// if(5 > 3){
+//     console.log("is greater than");
+// }
+
+//2
+// let cat = "cat"
+// if(cat.length === 3) {
+//     console.log("is the length");
+// }
+
+//3
+
+// let cat = "cat";
+// let dog = "dog";
+
+// if (cat === dog){
+//     console.log("they are the same");
+// } else {
+//     console.log("not the same");
+// }
+
+//bronze
+
+//1
+
+const person = {
+    name: "Bobby",
+    age: 12
+};
+
+// if(person.age > 18) {
+//     console.log(person.name + " is allowed to go to the movie");
+// } else {
+//     console.log(person.name + " is NOT allowed to go to the movie");
+// }
+
+// if(person.name[0] === "B") {
+//     console.log(person.name + " is allowed to go to the movie");
+// } else {
+//     console.log(person.name + " is NOT allowed to go to the movie");
+// }
+
+// if(person.name[0] === "B" && person.age > 18) {
+//     console.log(person.name + " is allowed to go to the movie");
+// } else {
+//     console.log(person.name + " is NOT allowed to go to the movie");
+// }
+
+//silver
+
+//1
+// if(1 === "1"){
+//     console.log("strict");
+// } else if(1 == "1"){
+//     console.log("loose");
+// } else {
+//     console.log("not equal at all");
+// }
+//2
+// if (1 <= 2 && 2 === 4){
+//     console.log("yes");
+// } else {
+//     console.log("no");
+// }
+
+//gold
+
+//1
+
+// if(typeof ("dog") === "string") {
+//     console.log("\"dog\" is a string");
+// } else {
+//     console.log("not a string");
+// }
+
+//2
+// if(typeof ("true") === "boolean") {
+//     console.log("\"true\" is a boolean");
+// } else {
+//     console.log("not a boolean");
+// }
+
+//3
+
+
+// console.log(typeof(x));
+
+// if(typeof(x) !== "undefined") {
+//     console.log("the variable is defined");
+// } else {
+//     console.log("the variable is undefined");
+// }
+
+// 4
+// let a = ("s" - 12);
+// if( "s" > 12) {
+//     console.log("true");
+// } else {
+//     console.log("not true");
+// }
+// console.log(typeof("s" > 12));
+
+//6
+let myNum = 4;
+
+let oddOrEven = (myNum % 2 === 0) ? "even" : "odd";
+
+console.log(oddOrEven);
+
+
+
+
