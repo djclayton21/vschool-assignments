498c154e10226a5802ae399dc70f43ef98d70938
Dan Clayton <clayton.daniel.j@gmail.com>
2019-08-23 16:00:27 -0600

    loops and arrays practice finish

--- Exercises/loops-conditionals-practice/loops-and-arrays-practice/app.js
+++ Exercises/loops-conditionals-practice/loops-and-arrays-practice/app.js
@@ -1,55 +1,78 @@
-//#1
+// //#1
 
-// let officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer",  "computer"]
-// let count = 0;
-// for(i = 0; i < officeItems.length; i++) {
-//     if(officeItems[i] === "computer"){
-//         count++;
-//     }
+// // let officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer",  "computer"]
+// // let count = 0;
+// // for(i = 0; i < officeItems.length; i++) {
+// //     if(officeItems[i] === "computer"){
+// //         count++;
+// //     }
+// // }
+// // console.log(count);
+
+// //#2
+
+// const peopleWhoWantToSeeMadMaxFuryRoad = [
+//   {
+//     name: "Mike",
+//     age: 12,
+//     gender: "male"
+//   },{
+//     name: "Madeline",
+//     age: 80,
+//     gender: "female"
+//   },{
+//     name: "Cheryl",
+//     age: 22,
+//     gender: "female"
+//   },{
+//     name: "Sam",
+//     age: 30,
+//     gender: "male"
+//   },{
+//     name: "Suzy",
+//     age: 4,
+//     gender: "female"
+//   }
+// ] 
+// let pronounObject = "They";
+// let pronounNoun = "Them";
+// let personGender = "a"
+// for(let i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++){
+//   personGender = peopleWhoWantToSeeMadMaxFuryRoad[i].gender;
+//   if(personGender === "male"){
+//     pronounObject = "him";
+//     pronounNoun = "He";
+//   } else {
+//     pronounObject = "her";
+//     pronounNoun = "She"
+//   }
+//   if(peopleWhoWantToSeeMadMaxFuryRoad[i].age > 18){
+//     console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is old enough. " + pronounNoun + "'s good to see Mad Max Fury Road")
+//   } else {
+//     console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is NOT old enough. Don't let " + pronounObject + " see it!")
+//   }
+
 // }
-// console.log(count);
 
-//#2
-
-const peopleWhoWantToSeeMadMaxFuryRoad = [
-  {
-    name: "Mike",
-    age: 12,
-    gender: "male"
-  },{
-    name: "Madeline",
-    age: 80,
-    gender: "female"
-  },{
-    name: "Cheryl",
-    age: 22,
-    gender: "female"
-  },{
-    name: "Sam",
-    age: 30,
-    gender: "male"
-  },{
-    name: "Suzy",
-    age: 4,
-    gender: "female"
+//Bonus Challenge
+function lightswitch(arr) {
+  let status = "off"
+  for (let i = 0; i < arr.length; i++){
+    for(let j = 0; j < arr[i]; j++){
+      if(status === "off"){
+        status = "on";
+      } else if(status === "on"){
+        status = "off";
+      } else {
+        status = "broken";
+      }
+    }
   }
-] 
-let pronounObject = "They";
-let pronounNoun = "Them";
-let personGender = "a"
-for(let i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++){
-  personGender = peopleWhoWantToSeeMadMaxFuryRoad[i].gender;
-  if(personGender === "male"){
-    pronounObject = "him";
-    pronounNoun = "He";
-  } else {
-    pronounObject = "her";
-    pronounNoun = "She"
-  }
-  if(peopleWhoWantToSeeMadMaxFuryRoad[i].age > 18){
-    console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is old enough. " + pronounNoun + "'s good to see Mad Max Fury Road")
-  } else {
-    console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is NOT old enough. Don't let " + pronounObject + " see it!")
-  }
-
+  return status
 }
+let arr1 = [2, 5, 435, 4, 3] // "The light is on"
+let arr2 = [1, 1, 1, 1, 3]   // "The light is on"
+let arr3 = [9, 3, 4, 2]      // "The light is off"
+console.log(lightswitch(arr1))
+console.log(lightswitch(arr2))
+console.log(lightswitch(arr3))
