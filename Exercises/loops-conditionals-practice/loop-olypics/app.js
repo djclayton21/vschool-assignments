e0e2783889be84f57302a2725f90dc5bae8f5cef
Dan Clayton <clayton.daniel.j@gmail.com>
2019-08-22 20:41:41 -0600

    finished loop olypics

--- Exercises/loops-conditionals-practice/loop-olypics/app.js
+++ Exercises/loops-conditionals-practice/loop-olypics/app.js
@@ -52,67 +52,126 @@
 
 //silver
 
-var peopleArray = [
-    {
-      name: "Harrison Ford",
-      occupation: "Actor"
-    },
-    {
-      name: "Justin Bieber",
-      occupation: "Singer"
-    },
-    {
-      name: "Vladimir Putin",
-      occupation: "Politician"
-    },
-    {
-      name: "Oprah",
-      occupation: "Entertainer"
-    }
-]
+// var peopleArray = [
+//     {
+//       name: "Harrison Ford",
+//       occupation: "Actor"
+//     },
+//     {
+//       name: "Justin Bieber",
+//       occupation: "Singer"
+//     },
+//     {
+//       name: "Vladimir Putin",
+//       occupation: "Politician"
+//     },
+//     {
+//       name: "Oprah",
+//       occupation: "Entertainer"
+//     }
+// ]
   
-//1
-// const names = [];
-// for(let i = 0; i < peopleArray.length; i++) {
-//     names.push(peopleArray[i].name);
-// }
-// console.log(names);
+// //1
+// // const names = [];
+// // for(let i = 0; i < peopleArray.length; i++) {
+// //     names.push(peopleArray[i].name);
+// // }
+// // console.log(names);
 
-//2
-// const names = [];
-// const occupations = [];
+// //2
+// // const names = [];
+// // const occupations = [];
+// // for(let i = 0; i < peopleArray.length; i++) {
+// //     names.push(peopleArray[i].name);
+// //     occupations.push(peopleArray[i].occupation);
+// // }
+// // console.log(names);
+// // console.log(occupations);
+
+// let names = [];
+// let occupations = [];
+
+// let nameIndex
+// let jobIndex
+
+// console.log(nameIndex < 1);
 // for(let i = 0; i < peopleArray.length; i++) {
-//     names.push(peopleArray[i].name);
-//     occupations.push(peopleArray[i].occupation);
+
+//     if(peopleArray[i].name === "Harrison Ford"){
+//         names.push(peopleArray[i].name);
+//         nameIndex = i;
+//     }
+//     if(peopleArray[i].occupation === "Singer"){
+//         occupations.push(peopleArray[i].occupation);
+//         jobIndex = i;
+//     }
+//     if(nameIndex < i && (i - nameIndex) % 2 === 0) {
+//         names.push(peopleArray[i].name);
+//     }
+//     if(jobIndex < i && (i- jobIndex) % 2 === 0) {
+//         occupations.push(peopleArray[i].occupation);
+//     }
 // }
+
+// console.log(nameIndex, jobIndex);
 // console.log(names);
 // console.log(occupations);
 
-const names = [];
-const occupations = [];
+//gold
+//1 
 
-let j 
-let k 
+// let grid = [];
+// for(i = 0; i < 3; i++){
+//     grid[i] = [];
+//     for(j = 0; j < 3; j++) {
+//       grid[i][j] = 0;
 
-console.log(k);
-for(let i = 0; i < peopleArray.length; i++) {
-     if (peopleArray[i].name === "Harrison Ford"){
-        names.push(peopleArray[i].name);
-        for(let j = i; j < peopleArray.length; j++) {
-            if((j - i) % 2 === 0) {
-                names.push(peopleArray[i].name);
-            }
-        }
-    } 
+//     }
+// }
+// console.log(grid);
 
-    if (peopleArray[i].occupation === "Singer"){
-        occupations.push(peopleArray[i].occupation);
-        for(let j = i; j < peopleArray.length; j++){
-            if((j - i) % 2 === 1){
-                occupations.push(peopleArray[i].occupation);
-            }
-        }
+//2
+// let grid = [];
+// for(i = 0; i < 3; i++){
+//     grid[i] = [];
+//     for(j = 0; j < 3; j++) {
+//       grid[i][j] = i;
+
+//     }
+// }
+// console.log(grid);
+
+//3
+// let grid = [];
+// for(i = 0; i < 3; i++){
+//     grid[i] = [];
+//     for(j = 0; j < 3; j++) {
+//       grid[i][j] = j;
+
+//     }
+// }
+// console.log(grid);
+
+// //4
+let grid = [];
+for(i = 0; i < 8; i++){
+    grid[i] = [];
+    for(j = 0; j < 2; j++) {
+      grid[i][j] = 0;
+
     }
 }
-console.log(names);
-console.log(occupations);
+console.log(grid);
+
+let gridX = grid;
+// console.log(gridX.length);
+// console.log(gridX[0].length);
+for (i = 0; i < gridX.length; i++) {
+  for( j = 0; j < gridX[0].length; j++){
+    gridX[i][j] = "x";
+  }
+}
+console.log(gridX);
+
+
+
