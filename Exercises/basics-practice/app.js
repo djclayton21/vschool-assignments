2d9166a5466d3e9c78b6ba6a4065335a2a7597d3
Dan Clayton <clayton.daniel.j@gmail.com>
2019-08-26 12:29:28 -0600

    dom debugging

--- Exercises/basics-practice/app.js
+++ Exercises/basics-practice/app.js
@@ -1,1 +1,19 @@
 // console.log("hello")
+
+//recursion?
+
+//date
+
+//convert date to day of the year
+function getDays(date1, date2) {
+    // console.log(date1)
+    let diff = date2 -date1;
+    diff = diff / (1000 * 60 * 60 * 24)
+	return diff
+}
+
+let test = getDays(new Date('July 20, 2019'), new Date('July 30, 2019'));
+console.log(test)
+
+
+//subtract
