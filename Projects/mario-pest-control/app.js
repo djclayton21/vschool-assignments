b107e38ad73ce6e39568aa19aa5253e87e297175
Dan Clayton <clayton.daniel.j@gmail.com>
2019-08-31 16:26:00 -0600

    mario small done

--- Projects/mario-pest-control/app.js
+++ Projects/mario-pest-control/app.js
@@ -6,6 +6,7 @@ function calculate(e){
     let goombasValue = document.form.goombas.value * 5;
     let bobombsValue = document.form.bobombs.value * 7;
     let cheepsValue = document.form.cheeps.value * 11;
+
     let totalValue = goombasValue + bobombsValue + cheepsValue;
     result.textContent = `${totalValue}`;
 
