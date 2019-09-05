3ff5d5b42a454f0517cea3749577068225690424
Dan Clayton <clayton.daniel.j@gmail.com>
2019-09-05 16:04:05 -0600

    higher order practice finish

--- Exercises/higher-order-practice/app.js
+++ Exercises/higher-order-practice/app.js
@@ -170,3 +170,185 @@
 //           age: 100
 //       }
 //   ]));
+
+//sort
+//1
+// function leastToGreatest(arr) {
+//   return arr.sort( (a,b) => a - b)
+// }
+
+// console.log(leastToGreatest([1, 3, 5, 2, 90, 20])); // [1, 2, 3, 5, 20, 90] 
+
+//2
+// function greatestToLeast(arr) {
+//   return arr.sort((a,b) => b - a)
+// }
+
+// console.log(greatestToLeast([1, 3, 5, 2, 90, 20])); // [90, 20, 5, 3, 2, 1] 
+
+//3
+// function lengthSort(arr) {
+//   return arr.sort((a, b) => a.length - b.length)
+// }
+
+// console.log(lengthSort(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "wolf", "eaten", "family"] 
+
+//4
+// function alphabetical(arr) {
+//     const sorted = arr.sort(function(a , b){
+//       return a[0].localeCompare(b[0])
+//     })
+//     return sorted
+// }
+
+// console.log(alphabetical(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "eaten", "family", "wolf"] 
+
+// //5
+// function byAge(arr){
+//     return arr.sort((p1, p2) => p1.age - p2.age)
+// }
+
+// console.log(byAge([
+//     {
+//         name: "Quiet Samurai",
+//         age: 22
+//     },
+//     {
+//         name: "Arrogant Ambassador",
+//         age: 100
+//     },
+//     {
+//         name: "Misunderstood Observer",
+//         age: 2
+//     },
+//     {
+//         name: "Unlucky Swami",
+//         age: 77
+//     }
+// ]));
+
+//reduce
+//1
+// function total(arr) {
+//   return arr.reduce((acc, num) => acc += num)
+// }
+
+// console.log(total([1,2,3])); // 6
+
+// //2
+// function stringConcat(arr) {
+//   return arr.reduce((str, num) => str + num, "")
+// }
+
+// console.log(stringConcat([1,2,3])); // "123"
+//3
+// function totalVotes(arr) {
+//   return arr.reduce((count, person) => {
+//     if (person.voted === true){
+//       count += 1
+//     } 
+//     return count
+//   },0)   
+// }
+
+// var voters = [
+//    {name:'Bob' , age: 30, voted: true},
+//    {name:'Jake' , age: 32, voted: true},
+//    {name:'Kate' , age: 25, voted: false},
+//    {name:'Sam' , age: 20, voted: false},
+//    {name:'Phil' , age: 21, voted: true},
+//    {name:'Ed' , age:55, voted:true},
+//    {name:'Tami' , age: 54, voted:true},
+//    {name: 'Mary', age: 31, voted: false},
+//    {name: 'Becky', age: 43, voted: false},
+//    {name: 'Joey', age: 41, voted: true},
+//    {name: 'Jeff', age: 30, voted: true},
+//    {name: 'Zack', age: 19, voted: false}
+// ];
+// console.log(totalVotes(voters)); // 7
+
+//4
+// function shoppingSpree(arr) {
+//   return arr.reduce((bill, item) => bill += item.price, 0)
+// }
+
+// var wishlist = [
+//    { title: "Tesla Model S", price: 90000 },
+//    { title: "4 carat diamond ring", price: 45000 },
+//    { title: "Fancy hacky Sack", price: 5 },
+//    { title: "Gold fidgit spinner", price: 2000 },
+//    { title: "A second Tesla Model S", price: 90000 }
+// ];
+
+// console.log(shoppingSpree(wishlist)); // 227005
+
+// //5
+// function flatten(arr) {
+//   return arr.reduce((newArr, subArr) => newArr.concat(subArr), [])
+// }
+
+// var arrays = [
+//    ["1", "2", "3"],
+//    [true],
+//    [4, 5, 6]
+// ];
+
+// console.log(flatten(arrays)); // ["1", "2", "3", true, 4, 5, 6];
+
+//6
+var voters = [
+  {name:'Bob' , age: 30, voted: true},
+  {name:'Jake' , age: 32, voted: true},
+  {name:'Kate' , age: 25, voted: false},
+  {name:'Sam' , age: 20, voted: false},
+  {name:'Phil' , age: 21, voted: true},
+  {name:'Ed' , age:55, voted:true},
+  {name:'Tami' , age: 54, voted:true},
+  {name: 'Mary', age: 31, voted: false},
+  {name: 'Becky', age: 43, voted: false},
+  {name: 'Joey', age: 41, voted: true},
+  {name: 'Jeff', age: 30, voted: true},
+  {name: 'Zack', age: 19, voted: false}
+];
+
+function voterResults(arr) {
+  return arr.reduce((result, voter) => {
+    if (18 <= voter.age && voter.age <= 25){
+      result.youth++
+      if (voter.voted === true){
+        result.youngVotes++
+      }
+    }
+    if (26 <= voter.age && voter.age <= 35){
+      result.mids++
+      if (voter.voted === true){
+        result.midVotes++
+      }
+    }
+    if (36 <= voter.age && voter.age <= 55){
+      result.olds++
+      if (voter.voted === true){
+        result.oldVotes++
+      }
+    }
+    return result
+  },{
+    youngVotes: 0,
+    youth: 0,
+    midVotes: 0,
+    mids: 0,
+    oldVotes: 0,
+    olds: 0
+  })
+}
+
+console.log(voterResults(voters)); // Returned value shown below:
+/*
+{ youngVotes: 1,
+youth: 4,
+midVotes: 3,
+mids: 4,
+oldVotes: 3,
+olds: 4 
+}
+*/
