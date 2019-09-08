06d7ea7c1dcc9b9c6b8bc570e00f17a906a7db53
Dan Clayton <clayton.daniel.j@gmail.com>
2019-09-08 15:43:19 -0600

    debugging and balance

--- Projects/RPG/app.js
+++ Projects/RPG/app.js
@@ -1,28 +1,30 @@
 const readline = require('readline-sync')
 //TODO//////////////
-//write content
-//level up function
-//balancing
 //graphics!
     //screen
         //text info
         //status
         //options
     //monster pictures
+//map
+    //update ending
+//more monsters
+//boss
 
 
 //////settings/////////////////////
-    const battleChance = 4 /// chance of an encounter, 1 in x
+    const battleChance = 3 /// chance of an encounter, 1 in x
 
     //battle balance, default to 10
-    const enemyDmgFactor = 10; ///increases max de
-    const enemyDefFactor = 10; //increases max attack
-    const playerDmgFactor = 10;
-    const playerDefFactor = 10;
+    const enemyDmgFactor = 15; ///increases max de
+    const enemyDefFactor = 15; //increases max attack
+    const playerDmgFactor = 15;
+    const playerDefFactor = 15;
 
     const keyFactor = 1; //increase chance of keys dropping
-    const levelUpFactor = 2;
-    const enemyBonusFactor = 1;
+    const levelUpGrowthFactor = 1;
+    const levelUpExperienceFactor = 2;
+    const enemySpawnBonus = 1;
 
 //variables////////////////////////////////////////////////////////////////
 
@@ -39,7 +41,7 @@ const player = {
     health: 500,
     maxHealth: 500,
     experience: 0,
-    levelUpXp: 500,
+    levelUpXp: 200,
     attack: 15,
     defense: 15,
     items: []
@@ -87,8 +89,8 @@ const minPotion = {
     quantity: 5
 }
 const maxPotion = {
-    name: "max power healing potion",
-    description: "A rare potion that will restore 1000 HP",
+    name: "full healing potion",
+    description: "A rare potion that will restore you to full health",
     useEffect: "restore hp",
     effectAmount: player.maxHealth,
     isUnique: false,
@@ -100,35 +102,35 @@ const enemies = [
         name: "Slime",
         baseLevel: 1,
         baseHealth: 10,
-        baseAttack: 1,
-        baseDefense: 1,
+        baseAttack: 5,
+        baseDefense: 5,
         description: "You step on a slime. Its not very threatening, but it won't let go of your boot. ",
         drops: [{item: rock, chance: 50},{item: minPotion, chance: 10}, {item: copperKey, chance: 20 * keyFactor}]
     },
     {
         name: "Goblin",
-        baseLevel: 2,
-        baseHealth: 150,
-        baseAttack: 15,
+        baseLevel: 3,
+        baseHealth: 250,
+        baseAttack: 20,
         baseDefense: 10,
         description: "A goblin was lying in wait. He looks annoying. ",
         drops: [{item: rock, chance: 50}, {item: minPotion, chance: 20}, {item: maxPotion, chance: 10}, {item: silverKey, chance: 20 * keyFactor}]
     },
     {
         name: "Kobold",
-        baseLevel: 2,
-        baseHealth: 150,
+        baseLevel: 3,
+        baseHealth: 250,
         baseAttack: 10,
-        baseDefense: 15,
+        baseDefense: 20,
         description: "A kobold attacks with a howl. He's fluffy and rather cute, but seems tough. ",
         drops: [{item: rock, chance: 50}, {item: minPotion, chance: 20}, {item: maxPotion, chance: 10}, {item: silverKey, chance: 20 * keyFactor}]
     },
     {
         name: "Orc",
-        baseLevel: 3,
-        baseHealth: 250,
-        baseAttack: 25,
-        baseDefense: 25,
+        baseLevel: 5,
+        baseHealth: 450,
+        baseAttack: 30,
+        baseDefense: 30,
         description: "An orc charges out of nowhere. He's terrifying, and smells faintly of bacon",
         drops: [{item: rock, chance: 50}, {item: minPotion, chance: 100}, {item: maxPotion, chance: 50}, {item: goldenKey, chance: 20 * keyFactor}]
     }   
@@ -139,9 +141,9 @@ const enemies = [
 
 //utility///////////////////////////////////
 function quit(){
-    screenText = 'Are you sure you want to quit the game? You cannot save. '
+    screenText = 'Are you sure you want to quit the game? You cannot save. [y/n] '
     console.log(screenText)
-    if (readline.keyInYNStrict()){
+    if (readline.keyInYNStrict('')){
         gameOver()
     }
 }
@@ -158,7 +160,7 @@ function rng(chance = 100){//return random number 0 to chance, default 100
 }
 
 function youDied(){
-    screenText = "As it turns out, you cannot live without blood. You're left wondering how things ended up this way, as the world fades into darkness. "
+    screenText = "The last of your life energy whithers away. As the world fades into darkness, your only regret is that you still don't know how you got here."
     console.log(screenText)
     readline.keyInPause()
 }
@@ -182,8 +184,11 @@ function checkVictory(){
         }
     }
     if (isCopper && isSilver && isGolden) {
-        victoryText("After picking up the third key, a door with three keyholes appears out of thin air. The keys turn smoothly, and you walk through into a comfortable, warm light. You hope someone on the other side has something to drink! ")
-        console.log("***** you found all the keys! You win!")
+        console.clear()
+        screenText = "After picking up the third key, a door with three keyholes appears out of thin air. The keys turn smoothly, and you walk through into a comfortable, warm light."
+        console.log(screenText)
+        readline.keyInPause()
+        screenText = "You still have no idea how you ended up in that forrest, but as the light embraces you, your only thought is that you're glad to have escaped."
         readline.keyInPause()
         gameOver()
     }
@@ -192,8 +197,9 @@ function checkVictory(){
 //move forward
 function forward(){
     //check path?
-    screenText = "You travel onward. "
+    screenText = "You travel onward."
     console.log(screenText)
+    readline.keyInPause()
     //encounter
     if (rng(battleChance) === 0) {
         encounter()
@@ -267,9 +273,8 @@ function itemUse(item){
 }
 //check status////////////////
 function status(){
-    //health
+    console.log(`${player.name} Lvl: ${player.level}`)
     console.log(`HP: ${player.health}/${player.maxHealth}`)
-    console.log(`Level: ${player.level}`)
     console.log(`XP: ${player.experience}/${player.levelUpXp}`)
     console.log(`Attack: ${player.attack}`)
     console.log(`Defense: ${player.defense}`)
@@ -354,11 +359,11 @@ function checkLevelUp(){
     if (player.experience >= player.levelUpXp) {
         player.level++
         player.experience+=  -player.levelUpXp
-        player.levelUpXp+= player.levelUpXp * (levelUpFactor / 2)
-        player.maxHealth+= rng(100 * levelUpFactor)
+        player.levelUpXp+= player.levelUpXp * (levelUpExperienceFactor / 2)
+        player.maxHealth+= rng(100 * levelUpGrowthFactor)
         player.health = player.maxHealth
-        player.attack+= rng(10 * levelUpFactor)
-        player.defense+= rng(10 * levelUpFactor)
+        player.attack+= rng(10 * levelUpGrowthFactor)
+        player.defense+= rng(10 * levelUpGrowthFactor)
 
         screenText = `You are now level ${player.level}!`
         console.log(screenText)
@@ -367,10 +372,11 @@ function checkLevelUp(){
 }
 function Enemy(enemyObj){
     this.name = enemyObj.name
-    this.health = enemyObj.baseHealth + rng(100 * enemyBonusFactor)
+    this.level = enemyObj.baseLevel
+    this.health = enemyObj.baseHealth + rng(100 * enemySpawnBonus)
     this.maxHealth = this.health
-    this.attack = enemyObj.baseAttack + rng(10 * enemyBonusFactor)
-    this.defense = enemyObj.baseDefense + rng(10 * enemyBonusFactor)
+    this.attack = enemyObj.baseAttack + rng(10 * enemySpawnBonus)
+    this.defense = enemyObj.baseDefense + rng(10 * enemySpawnBonus)
     this.description = enemyObj.description
     this.drops = enemyObj.drops
 }
@@ -379,11 +385,11 @@ Enemy.prototype.experience = function () {
     return xp
 }
 function battleRound(enemy){
-    console.clear
     menuText = '[a] Attack [r] Run [i] Inventory [q] quit game '
     console.log(menuText)
     let choice = 0
     choice = readline.keyIn('>>>', {limit: ['a','r','i','q']})
+    console.clear()
     if (choice === 'q'){
         quit()
     } else if (choice === 'a'){
@@ -445,15 +451,17 @@ console.log(screenText)
 readline.keyInPause()
 
 //get name////////////////////
-player.name = readline.question('What is your name?')
+console.clear()
+player.name = readline.question('What is your name? ')
 screenText = `OK. ${player.name} seems like a good strong name. You'll go with that for now. With that decided, you set off on your journey...`
-
+console.log(screenText)
+readline.keyInPause()
 player.items.push(Object.assign({}, minPotion))///start with healing potions
 
 //walking around
 while (!gameover) {
     console.clear()
-    screenText = "You take a break. "
+    screenText = "Little has changed. You contemplate your next move."
     console.log(screenText)
     menuText = '[w] Move Forward [s] Status [i] Inventory [q] quit'
     console.log(menuText)
