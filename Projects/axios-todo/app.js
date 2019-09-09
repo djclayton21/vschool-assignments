1f1dfe35c4768127555ed401c20f22c3301ee432
Dan Clayton <clayton.daniel.j@gmail.com>
2019-09-09 16:18:37 -0600

    get connection working

--- Projects/axios-todo/app.js
+++ Projects/axios-todo/app.js
@@ -1,1 +1,33 @@
-const axios = require('axios')
+//grab todo container
+const listContainer = document.getElementById("list-container")
+//GET todos
+    //get data
+    axios.get("https://api.vschool.io/Dan/todo/")
+    .then(res => {
+        const todos = res.data
+        todos.forEach(createTodo)
+    })
+    .catch(err => alert(err))
+    //put data on site
+    function createTodo(todo) {
+        todoContainer = document.createElement("div")
+        console.log(todoContainer)
+
+        //name
+        const todoName = document.createElement('h3')
+        todoName.textContent = todo.title
+        todoContainer.appendChild(todoName)
+        //picture
+        //desription
+        //price
+        
+        //comleted? -> strikethrough
+        listContainer.appendChild(todoContainer)
+    }
+
+//POST new todos
+
+//PUT complete or incomplete
+
+//DELETE old todos
+
