f53a2a3cd51e314f1a4ac7f16bd0cb1f60b7d94a
Dan Clayton <clayton.daniel.j@gmail.com>
2019-10-10 10:42:27 -0600

    bounty hunter work

---
+++ Exercises/original-bounty-hunter/client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './style.css';
+import axios from 'axios'
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bounties: []
+        }
+    }
+
+    componentDidMount(){
+        axios.get('/bounties')
+            .then(res => {
+                this.setState({bounties:[...res.data]})
+            })
+            .catch(err => console.log(err))
+    }
+    render() { 
+        const mappedBounties = this.state.bounties.map(bounty => <div className = 'bounty' key = {`${bounty._id}`} >{`Bounty: ${bounty.firstName} ${bounty.lastName}`}</div>)
+        return (
+            <div className="app">
+                {mappedBounties}
+            </div>
+        );
+    }
+}
+ 
+export default App;
